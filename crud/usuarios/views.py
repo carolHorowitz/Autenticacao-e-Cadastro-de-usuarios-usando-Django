c9cb001a77a800c6cc872c7ae66df74936a98e84
Carol Horowitz <carolvet.horowitz@gmail.com>
2023-01-20 17:51:23 -0300

    Criando a primeira view - view index

--- crud/usuarios/views.py
+++ crud/usuarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Bem vindo ao cadastro do usuários")
+
 
 # Create your views here.
