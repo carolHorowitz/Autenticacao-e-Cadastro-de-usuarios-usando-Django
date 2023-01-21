2769dbae43fce522f4de2862bd13382b6800f2b0
Carol Horowitz <carolvet.horowitz@gmail.com>
2023-01-21 15:11:31 -0300

    Iniciando criacao do banco de dados

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios.apps.UsuariosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- crud/usuarios/models.py
+++ crud/usuarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=200, null=False, blank=False)
+    telefone = models.CharField(max_length=10, null=False, blank=False)
+    email = models.CharField(max_length=30, null=False, blank=False)
+    cpf = models.CharField(max_length=11, null=False, blank=False)
+    data_nascimento = models.CharField(max_length=10, null=False, blank=False)
+     
+    def __str__(self):
+        return self.nome, self.telefone
+
+
+
+
