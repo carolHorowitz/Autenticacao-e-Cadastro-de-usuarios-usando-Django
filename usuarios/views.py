a03ec3921ac8d74368f0a67549efeea89c38dd9b
Carol Horowitz <carolvet.horowitz@gmail.com>
2023-02-04 17:24:23 -0300

    Alterando páginas de autenticacao de usuarios - incluidas paginas de cadastro e login

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 from .models import Usuario
 
-def index(request):
-    name_list = Usuario.objects.all()
-    context = {'name_list': name_list,}
-    return render(request, 'usuarios/index.html', context)
+def cadastro(request):
+    return render(request, 'cadastro.html')
 
+def login(request):
+    return render(request, 'login.html')    
    
 
 
