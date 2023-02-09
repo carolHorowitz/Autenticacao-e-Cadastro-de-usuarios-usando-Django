0a6af0258776cc68f7748e20fe3d611debabfbe1
Carol Horowitz <carolvet.horowitz@gmail.com>
2023-02-08 21:33:54 -0300

    Created main page with HyperLinks to redirect user

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index')
 ]
