391bb8f792facf36c825a71911533e8bc9a89d99
Carol Horowitz <carolvet.horowitz@gmail.com>
2023-02-13 22:02:59 -0300

    páginas de login e cadastro funcionando; implementado autenticacao de usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Usuario
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 def cadastro(request):
     if request.method == "GET":
@@ -8,11 +12,39 @@ def cadastro(request):
     else:
         username = request.POST.get('username')
         email = request.POST.get('email')
-        senha = request.POST.get('senha')
-        return HttpResponse(username)    
+        password = request.POST.get('password')
+        
+        user = User.objects.filter(username=username).first()
+        
+        if user:
+            return HttpResponse("Já existe um usuário cadastrado com esse username.")
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        
+        return HttpResponse("Usuário cadastrado com sucesso")
+        
 
 def login(request):
-    return render(request, 'login.html')    
-   
+    if request.method == "GET":
+        return render(request, 'login.html')
+    
+    else: 
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            login_django(request, user)
+            
+            return HttpResponse("Autenticado")
+        else:
+            return HttpResponse("Usuário ou Senha inválidos") 
+        
+@login_required(login_url="/usuarios/login/")
+def plataforma(request):
+    return HttpResponse("Plataforma")
+    
 
 
